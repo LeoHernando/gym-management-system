fc3f7c9110c4e1459dc7056a6c3c2dc35b864069
Leo <l.hernando.tan@gmail.com>
2026-02-19 16:50:09 +0800

    validate jwt token

--- auth-service/src/main/java/com/leohernando/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/leohernando/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.leohernando.authservice.service;
 import com.leohernando.authservice.dto.LoginRequestDTO;
 import com.leohernando.authservice.model.User;
 import com.leohernando.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,14 @@ public class AuthService {
                         .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
         return token;
 
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
